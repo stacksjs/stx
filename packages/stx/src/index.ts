cbffab121deabcc97cc834a7b794d3a25a0dce59
Chris <chrisbreuer93@gmail.com>
2025-05-03 14:24:35 -0700

    chore: remove comments

--- packages/stx/src/index.ts
+++ packages/stx/src/index.ts
@@ -8,13 +8,11 @@ export * from './docs'
 export * from './expressions'
 export * from './forms'
 
-// Export i18n functions for advanced usage
 export { createTranslateFilter, getTranslation, loadTranslation, processTranslateDirective } from './i18n'
 
 export * from './includes'
 export * from './loops'
 
-// Export markdown functions for advanced usage
 export { markdownDirectiveHandler, processMarkdownDirectives } from './markdown'
 
 export * from './middleware'
@@ -29,7 +27,6 @@ export type { ComponentDoc, ComponentPropDoc, CustomDirective, DirectiveDoc, Doc
 export * from './types'
 export * from './utils'
 
-// Export web component functions for advanced usage
 export { buildWebComponents, webComponentDirectiveHandler } from './web-components'
 
 export default plugin
