39e9fae853dc07b36016618f560a37a811d9f02f
Adelino Ngomacha <Adelinob335@gmail.com>
2025-07-08 19:52:58 +0200

    chore: update pre commit stagedLint

--- git-hooks.config.ts
+++ git-hooks.config.ts
@@ -2,7 +2,14 @@
 import type { GitHooksConfig } from 'bun-git-hooks'
 
 const config: GitHooksConfig = {
-  'pre-commit': 'bun run lint && bun run test',
+  'pre-commit': {
+    stagedLint: {
+      '*.js': 'eslint --fix',
+      '*.{ts,tsx}': ['eslint --fix', 'prettier --write'],
+      '*.css': 'stylelint --fix',
+      '*.md': 'prettier --write'
+    }
+  },
   'commit-msg': 'bun commitlint --edit $1',
   'pre-push': 'bun run build',
   'verbose': true,
