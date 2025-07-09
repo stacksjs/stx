98d94cbb90caabf0af1356ceaec3012d93ba9a68
Adelino Ngomacha <Adelinob335@gmail.com>
2025-07-09 14:18:30 +0200

    feat: update hooks and scripts to use bun directly

--- git-hooks.config.ts
+++ git-hooks.config.ts
@@ -1,21 +1,21 @@
 // git-hooks.config.ts
-import type { GitHooksConfig } from 'bun-git-hooks'
+import type { GitHooksConfig } from "bun-git-hooks";
 
 const config: GitHooksConfig = {
-  'pre-commit': {
-    'staged-lint': {
-      '*.{js,ts,tsx,stx}': './node_modules/.bin/eslint --fix',
-      '*.{ts,tsx,stx}': [
-        './node_modules/.bin/eslint --fix',
-        './node_modules/.bin/prettier --write',
+  "pre-commit": {
+    "staged-lint": {
+      "*.{js,ts,tsx,stx}": "bun eslint --fix",
+      "*.{ts,tsx,stx}": [
+        "bun eslint --fix",
+        "bun prettier --write",
       ],
-      '*.css': './node_modules/.bin/stylelint --fix',
-      '*.md': './node_modules/.bin/prettier --write',
+      "*.css": "bun stylelint --fix",
+      "*.md": "bun prettier --write",
     },
   },
-  'commit-msg': 'bun node_modules/@stacksjs/gitlint/dist/bin/cli.js --edit $1',
-  'post-commit': 'bun run changelog:generate',
-  'pre-push': 'bun run build',
-}
+  "commit-msg": "bun node_modules/@stacksjs/gitlint/dist/bin/cli.js --edit $1",
+  "post-commit": "bun run changelog:generate",
+  "pre-push": "bun run build",
+};
 
-export default config
+export default config;
