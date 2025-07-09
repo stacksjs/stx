4ce0264f7b209f053c0d6c4828c31f69b8e32937
Adelino Ngomacha <Adelinob335@gmail.com>
2025-07-09 14:53:08 +0200

    feat: clean up scripts and optimize git hooks workflow

--- git-hooks.config.ts
+++ git-hooks.config.ts
@@ -1,18 +1,17 @@
 // git-hooks.config.ts
-import type { GitHooksConfig } from "bun-git-hooks";
+import type { GitHooksConfig } from 'bun-git-hooks'
 
 const config: GitHooksConfig = {
-  "pre-commit": {
-    "staged-lint": {
-      "*.{js,ts,tsx,stx}": "bun eslint --fix",
-      "*.{ts,tsx,stx}": ["bun eslint --fix", "bun prettier --write"],
-      "*.css": "bun stylelint --fix",
-      "*.md": "bun prettier --write",
+  'pre-commit': {
+    'staged-lint': {
+      '*.{js,ts,tsx,stx}': 'bun eslint --fix',
+      '*.css': ['bun stylelint --fix', 'bun run lint:fix'],
+      '*.md': ['bun prettier --write', 'bun run lint:fix'],
     },
   },
-  "commit-msg": "bun node_modules/@stacksjs/gitlint/dist/bin/cli.js --edit $1",
-  "post-commit": "bun run changelog:generate",
-  "pre-push": "bun run build",
-};
+  'commit-msg': 'bun node_modules/@stacksjs/gitlint/dist/bin/cli.js --edit $1',
+  'post-commit': 'bun run changelog:generate',
+  'pre-push': 'bun run build',
+}
 
-export default config;
+export default config
