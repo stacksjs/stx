a190203633e193e860c5648ebbc4b3dccab8f14d
Chris <chrisbreuer93@gmail.com>
2026-01-28 10:47:58 -0800

    chore: wip

---
+++ packages/vscode/src/services/index.ts
@@ -0,0 +1,4 @@
+export { ComponentRegistry } from './ComponentRegistry'
+export type { ComponentInfo } from './ComponentRegistry'
+export { PropsTypeExtractor } from './PropsTypeExtractor'
+export type { PropDetail, ExtractedProps } from './PropsTypeExtractor'
